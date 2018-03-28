48698581a493fe6b28e4c9a9f1e40e91c5f78251
serg495 <serg@localhost>
2018-03-28 15:52:47 +0300

    Make auth, set db, add modules

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'phone_number', 'address'
     ];
 
     /**
@@ -26,4 +26,60 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function generatePassword($password)
+    {
+        if (isset($password)) {
+            $this->password = bcrypt($password);
+            $this->save();
+        }
+    }
+
+    public static function add($fields)
+    {
+        $user = new static;
+        $user->fill($fields);
+        $user->save();
+
+        return $user;
+    }
+
+    public function edit($fields)
+    {
+        $this->fill($fields);
+        $this->save();
+    }
+
+    public function remove()
+    {
+        $this->removeAvatar();
+        $this->delete();
+    }
+
+    public function uploadAvatar($image)
+    {
+        if ($image == null) { return; }
+        $this->removeAvatar();
+        $filename = str_random(10) . '.' . $image->extension();
+        $image->storeAs('uploads', $filename);
+        $this->avatar = $filename;
+        $this->save();
+    }
+
+    public function removeAvatar()
+    {
+        if (isset($this->avatar)){
+            Storage::delete('uploads/' . $this->avatar);
+        }
+    }
+
+    public function getAvatar()
+    {
+        if ($this->avatar == null) {
+            return '/img/no-user-image.png';
+        } else {
+            return '/uploads/' . $this->avatar;
+        }
+    }
 }
+
